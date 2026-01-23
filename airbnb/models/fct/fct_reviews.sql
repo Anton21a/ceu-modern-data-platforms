046c2a489e7a1f9e626eb84c80c2f8eab3aced3c
Zoltan C. Toth <zoltan@zoltanctoth.com>
2026-01-23 16:13:29 +0100

    Revert changes to fct_reviews.sql and sources.yml

--- airbnb/models/fct/fct_reviews.sql
+++ airbnb/models/fct/fct_reviews.sql
@@ -6,12 +6,9 @@
 }}
 
 WITH src_reviews AS (
-  SELECT * FROM {{ ref('src_reviews') }}
+  SELECT *, current_timestamp() AS loaded_at FROM {{ ref('src_reviews') }}
 )
-SELECT
-  {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} as review_id,
-  *,
-  current_timestamp() AS loaded_at
+SELECT * 
 FROM src_reviews
 WHERE review_text is not null
 {% if is_incremental() %}
